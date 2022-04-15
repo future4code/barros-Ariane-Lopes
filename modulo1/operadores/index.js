1b345ff0e53771f1dfa774fd04f3c8e029cf6e0c
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2022-04-14 23:08:10 -0300

    adicionar Index HTML js

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,148 @@
+// -------------EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO--------
+// 1.
+
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2  // (!) SEMPRE RETORNA O OPOSTO, ENTÃO ELE É 'TRUE'.
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
+
+'a'. false
+'b'. false
+'c'. true
+'d' .boolean */
+
+//=======================================================================================
+
+/*2.
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) 
+
+'TERIA QUE TRANSFORMAR A STRING EM NUMBER'
+
+===========================================================================================*/
+
+//3.
+
+
+/*let primeiroNumero = Number (prompt ("Digite um numero!"))
+let segundoNumero = Number (prompt ("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) */
+
+//===========================================================================================
+
+
+// EXERCÍCIO DE ESCRITA DE CODIGO
+
+// 1;A, B, C, D
+
+/*let idadeDoUsuario = Number (prompt ('Digite a idade do usuario')) 
+let melhorAmiga = Number (prompt ('Digite a idade da melhor amiga ')) 
+let maiorIdade = idadeDoUsuario >= melhorAmiga 
+
+console.log(maiorIdade)
+
+//RESPOSTA DA C= FALSE
+let diferencaDeIdade = (idadeDoUsuario - melhorAmiga)
+
+console.log(diferencaDeIdade)*/
+//RESPOSTA DA D: -9
+
+//================================================================
+
+//2.a,b
+
+/*let restoDaDivisao = 58 % 2
+console.log(restoDaDivisao) */
+
+//=============================================================
+/*c. Todos os resto da divisão de numeros pares é igual a 0.
+==============================================================
+d.Os numeros ímpares tem "sobras".
+=============================================================*/
+
+//3.
+
+
+/*let idadeEmAnos = Number (prompt('Digite a idade em anos'))
+let idadeEmMeses = Number (prompt('Digite a idade em meses'))
+let idadeEmDias = Number (prompt('Digite a idade em dias'))
+let idadeEmHoras = Number (prompt('Digite a idade em horas'))
+
+console.log(idadeEmAnos)
+console.log(idadeEmMeses)
+console.log(idadeEmDias)
+console.log(idadeEmHoras)*/
+
+//4.
+
+/*let primeiroNumero = Number (prompt('Digite um numero'))
+let segundoNumero = Number (prompt('Digite outro numero'))
+
+console.log(primeiroNumero) 
+
+console.log(segundoNumero) 
+
+const primeiroNumero1 = 32
+const segundoNumero2 = 8
+
+//O primeiro numero é maior que segundo? true
+
+let comparacaoEntreNumeros = primeiroNumero1 > segundoNumero2 
+
+console.log(comparacaoEntreNumeros)
+
+
+/*
+O primeiro numero é igual ao segundo? false 
+
+let numeroIgual = primeiroNumero1 === segundoNumero2
+
+console.log(numeroIgual)
+
+
+
+
+/*
+O primeiro numero é divisível pelo segundo? true  
+
+let primeiroDivisivel = primeiroNumero1 % segundoNumero2 === 0
+
+console.log(primeiroDivisivel)
+
+
+/*O segundo numero é divisível pelo primeiro? true 
+
+let segundoDivisivel = segundoNumero2 % primeiroNumero1 === 0
+
+console.log(segundoDivisivel)
+
+
+
+
+
+
+
+
+
+
+
+
+
