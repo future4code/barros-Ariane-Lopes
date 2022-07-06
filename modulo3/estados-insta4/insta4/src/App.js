6a8f342ad053d17f9006c7c74feb0dda2fc0da71
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2022-07-05 22:01:25 -0300

    estados-insta4

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -4,37 +4,24 @@ import './style.css'
 
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
+  const listaPost = [
+    {nomeUsuario:'paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
+    {nomeUsuario:'Luiz Rafhael', fotoUsuario:'https://picsum.photos/51/52', fotoPost:'https://picsum.photos/250/180'},
+    {nomeUsuario:'Ariane', fotoUsuario:'https://picsum.photos/70/50', fotoPost:'https://picsum.photos/100/150'},
+    {nomeUsuario:'Benedito', fotoUsuario:'https://picsum.photos/100/50', fotoPost:'https://picsum.photos/300/150'},
+  ]
 
-          <Post
-            nomeUsuario={'Luiz Rafhael'}
-            fotoUsuario={'https://picsum.photos/51/52'}
-            fotoPost={'https://picsum.photos/250/180'}
-          />
+  const listaInformcoes = listaPost.map((elemento, index)=>{
+    return <Post nomeUsuario={elemento.nomeUsuario}
+    fotoUsuario={elemento.fotoUsuario}
+    fotoPost={elemento.fotoPost}
+    key={index} />
+  })
 
-          <Post
-            nomeUsuario={'Ariane'}
-            fotoUsuario={'https://picsum.photos/70/50'}
-            fotoPost={'https://picsum.photos/100/150'}
-          />
-
-          <Post
-            nomeUsuario={'Benedito'}
-            fotoUsuario={'https://picsum.photos/100/50'}
-            fotoPost={'https://picsum.photos/300/150'}
-          />
-  </div>
-
-  
-)
-
-}
-
-
+  return(
+    <div className='MaiContainer'>
+      {listaInformcoes}
+      {console.log(listaPost)}
+    </div>
+  )}
 export default App;
