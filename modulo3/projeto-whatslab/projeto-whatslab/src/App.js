a205df120f2fc23192af70ec06d57501aee3bf1e
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2022-07-10 20:18:02 -0300

    projeto-whatslab

--- modulo3/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {AreaMensagem, PostContainer, PostForm, PostMsg,} from './style';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [postar, setPostar] = useState([
+    ]);
 
+const itensPostar = postar.map((item, index) => {
+    return (
+        <PostMsg onDoubleclick = {() => remover(index)} key={index}>
+
+            <p><strong>{item.usuario}</strong></p>
+            <p>{item.mensagem}</p>
+
+        </PostMsg>
+
+    );
+});
+const [ImputUser, setImputUser]=useState ("");
+const [ImputMsg, setImputMsg]=useState("");
+
+const mudaUsuar=(e) =>{
+    setImputUser(e.target.value);
+    console.log();
+    console.log(ImputUser);
+}
+const mudaMsg=(e)=>{
+    setImputMsg(e.target.value)
+    console.log(ImputMsg);
+}
+const adicionar=(e)=>{
+    e.preventDefault()
+    const itensAtualizados=[...postar,{usuario:ImputUser,mensagem:ImputMsg}]
+    setPostar(itensAtualizados)
+    console.log("botão precionado!");
+    setImputUser("")
+    setImputMsg("")
+}
+const remover=(indexRemover)=>{
+    const itensAtualizados=postar.filter((item,index)=>{
+        return index!==indexRemover;
+    })
+    setPostar(itensAtualizados)
+}
+    return (
+        <>
+        <AreaMensagem>
+                <PostForm>
+                    <p></p>
+                    <imput type="text" required placeholder="usuario" value={ImputUser} onChange={mudaUsuar}></imput>
+                    <imput type="text" required placeholder="Mensagem" value={ImputMsg} onChange={mudaMsg}></imput>
+                    <button onClick={adicionar}>Enviar </button>
+                </PostForm>
+                <PostContainer>
+            <span>
+                <ul>{itensPostar}</ul>
+            </span>
+        </PostContainer>
+
+        </AreaMensagem>
+        </>
+    );
+}
+   
 export default App;
