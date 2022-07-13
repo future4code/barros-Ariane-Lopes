0bae38844824599f4045a316641f8e55ebea9590
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2022-07-12 22:33:28 -0300

    Labeforms

---
+++ modulo3/rend-condicional-form/rend-condicional/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Etapa1 from "./componentes/etapa1"
+
+function App() {
+  return(
+    <div className="App">
+      <Etapa1/>
+    </div>
+
+  )
+}
+
+export default App;
---
+++ modulo3/rend-condicional-form/rend-condicional/src/componentes/etapafinal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EtapaFinal (){
+    return(
+        <Login>
+            <h1>O FORMULÁRIO ACABOU</h1>
+            <h3>Muito obrigado por participar! Entraremos em contato!</h3>
+        </Login>
+    )
+    }
+
+export default EtapaFinal;
